a7441ac16dfb8f59f6e299a7d890afe948a959ae
Pernilla <pernilla.z@gmail.com>
2016-03-10 13:02:26 +0100

    images and Game image view

--- Miniprojekt/Controllers/GameImageController.cs
+++ Miniprojekt/Controllers/GameImageController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Miniprojekt.ViewModels;
 
 namespace Miniprojekt.DataAccess
 {
     public class GameImageController : Controller
     {
+
         // GET: GameImage
         public ActionResult Index()
         {
-            return View();
+			var model = new ImageViewModel();
+            return View(model);
         }
     }
 }
---
+++ Miniprojekt/ViewModels/ImageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Miniprojekt.ViewModels
+{
+	public class ImageWordPair
+	{
+		public string imageUrl;
+		public string imageWord;
+	}
+
+	public class ImageViewModel
+	{
+		public List<ImageWordPair> ImageList { get; set; }
+
+		public ImageViewModel()
+		{
+			ImageList = new List<ImageWordPair>();
+			ImageList.Add(new ImageWordPair { imageUrl = "/Content/Images/Games/bild1.jpg", imageWord = "KATT" });
+			ImageList.Add(new ImageWordPair { imageUrl = "/Content/Images/Games/bild2.jpg", imageWord = "FOTBOLL" });
+			ImageList.Add(new ImageWordPair { imageUrl = "/Content/Images/Games/bild3.jpg", imageWord = "CYKEL" });
+			ImageList.Add(new ImageWordPair { imageUrl = "/Content/Images/Games/bild4.jpg", imageWord = "FISK" });
+			ImageList.Add(new ImageWordPair { imageUrl = "/Content/Images/Games/bild5.jpg", imageWord = "HUS" });
+			ImageList.Add(new ImageWordPair { imageUrl = "/Content/Images/Games/bild6.jpg", imageWord = "ANKA" });
+			ImageList.Add(new ImageWordPair { imageUrl = "/Content/Images/Games/bild7.jpg", imageWord = "FÅR" });
+			ImageList.Add(new ImageWordPair { imageUrl = "/Content/Images/Games/bild8.png", imageWord = "NYCKELPIGA" });
+			ImageList.Add(new ImageWordPair { imageUrl = "/Content/Images/Games/bild9.jpg", imageWord = "GLASÖGON" });
+			ImageList.Add(new ImageWordPair { imageUrl = "/Content/Images/Games/bild10.jpg", imageWord = "STOL" });
+		}
+	}
+}
